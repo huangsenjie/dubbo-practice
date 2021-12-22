5bd7e30e96a3ba7d3a07cf3c4fd9ef3a06d99eda
huangsenjie <huangsenjie@eteams.cn>
2021-12-22 10:49:43 +0800

    first commit

---
+++ src/main/java/com/hdk/dubbopractice/DubboPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.hdk.dubbopractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubboPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DubboPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hdk/dubbopractice/DubboPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hdk.dubbopractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DubboPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
